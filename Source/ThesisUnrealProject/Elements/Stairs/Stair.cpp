0a4c10c54f837ed1119ae0a7741cb7524e2f1c76
Diego <diego.vallauri@gmail.com>
2021-08-23 00:39:25 +0200

    Added stairs

---
+++ Source/ThesisUnrealProject/Elements/Stairs/Stair.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Stair.h"
+
+// Sets default values
+AStair::AStair()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Instance = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Root"));
+    RootComponent = Instance;
+
+	StepsNumber = 8;
+
+}
+
+// Called when the game starts or when spawned
+void AStair::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	for(int i = 0; i < StepsNumber; i++){
+		
+		FTransform Transform;
+		FVector SpawnLocation(0.f + 500.f * i,0.f,0.f - 100.f * i);
+		Transform.SetLocation(SpawnLocation);
+		Instance->AddInstance(Transform);
+
+	}
+
+}
+
+// Called every frame
+void AStair::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
